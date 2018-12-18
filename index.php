57f6ec1270ce250d14f63514d8ddf5addcf192ba
malick douet <buzzi@MacBook-Air-de-malick.local>
2018-12-18 01:12:36 +0100

    Update qcm answer

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,31 +8,70 @@
     <title>QCM</title>
 </head>
 <body>
+
+    <?php
+        $score = 0;
+        if(!empty($_POST)) { // si validation du formulaire
+            $answers_sent = $_POST;
+            $answers = $_SESSION['reponses'];
+            // echo '<pre>', print_r($_POST, true), '</pre>'; // les réponses envoyés
+            // echo '<pre>', print_r($_SESSION['reponses'], true), '</pre>'; // les vrais réponses
+            
+
+            foreach($answers as $key => $value) {
+                if ($value == $answers_sent[$key]) {
+                    $score++;
+                }
+            }
+        }
+    ?>
     <h1> QCM </h1>
     <div class="box">
+    <?php if(!empty($_SESSION['reponses'])): ?>
+        <p>Score : <?= $score?> / <?= count($_SESSION['reponses']) ?></p>
+    <?php endif; ?>
     <?php
-        $txt = fopen ("text.txt", "r"); //ouvrir le fichier
-        
-        $i=0; 
+        $txt = fopen("qcm.txt", "r"); ?> //ouvrir le fichier
+    <form method="POST">
+    <?php
+        $i = 1;
+        $responses = [];
         while (FALSE !== ($line = fgets($txt))){ //cette ligne permet de revenir à la ligne apres avoir lu la ligne tant qu'il y a des lignes à lire.
-            $tableau=explode("##",$line);
-    
-            echo("<tr>");
-            echo("<td>"."Question n°".$i."<br></td>");
-            echo("<td> <p class=\"question\">".$tableau[0]."</p><br></td>");
-            echo("<td> <p class=\"question\">".$tableau[1]."</p><br></td>");
-            echo("<td> <p class=\"question\">".$tableau[2]."</p><br></td>");
-            echo("<td> <p class=\"question\">".$tableau[3]."</p><br></td>");
-            echo("<td> <p class=\"question\">".$tableau[4]."</p><br></td>");
-            echo("<td> <p class=\"question\">".$tableau[5]."</p><br></td>");
-            $i++;
-            echo("<br><br>");
-            echo("</tr>");
+            $line = explode("##", $line);
+
+            // echo '<pre>', print_r($line, true), '</pre>';
+             $question = array_shift($line); // récupérer le premier element du tableau (la question)
+
+             // récupérer la bonne réponse
+             echo "Question n°".$i.": ";
+             echo $question . '<br>';
             
+             foreach ($line as $key => $question) {
+                if (strpos($question, "(") !== false) {
+                    // on peut utiliser un preg_replace 
+                    $question = str_replace(")", "", $question);
+                    $question = str_replace("(", "", $question);
+
+                    // stockage des réponses respectives
+                    $responses['answer'.$i] = $question; // reponse
+                }
+                echo "<label>";
+                echo '<input type="radio" name="answer'.$i.'" value="' . $question . '">';
+                echo $question;
+                echo "</label><br>";
+             }
+             echo "<hr>";
+             $i++;
         }
+
+        if (empty($_SESSION['reponses'])) {
+            $_SESSION['reponses'] = $responses;
+        }
+        // echo '<pre>', print_r($responses, true), '</pre>';
     
-?>
-    
+    ?>
+    <input type="submit" value="Envoyer les réponses">
+    </form>
 </div>
 </body>
 </html>
